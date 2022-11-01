62e2bf8de9e2b09ba6bd712d1e236a10af4b6b0d
xsklorei <jcvxka@gmail.com>
2022-11-01 11:37:14 -0700

    Added Tile

--- RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
+++ RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position; 
     }
 }
