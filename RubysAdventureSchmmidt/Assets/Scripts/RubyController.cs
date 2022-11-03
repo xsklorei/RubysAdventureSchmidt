8783214b283ace1f7eebb80928de8e05f0733455
xsklorei <jcvxka@gmail.com>
2022-11-03 11:43:25 -0700

    Added Tilemap
    Added tiles and fixed ruby's movement with comments

--- RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
+++ RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
@@ -13,12 +13,18 @@ public class RubyController : MonoBehaviour
  
     void Update()
     {
+        //create two variables
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        //create movement vector
         Vector2 position = transform.position;
+
+        //changing x, y position
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //set the updated position
         transform.position = position; 
     }
 }
