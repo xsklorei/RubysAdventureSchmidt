e785c49edfc9b76a07b08983051b036c1ff753c8
xsklorei <jcvxka@gmail.com>
2022-11-18 11:40:53 -0800

    Added Colliders
    Done with World Interactions Blocking Movement up to 11.

--- RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
+++ RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
 
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
- 
+
     void Update()
     {
         //create two variables
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         //create movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //changing x, y position
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
 
         //set the updated position
-        transform.position = position; 
+        rigidbody2d.MovePosition(position);
     }
+
+
+
+   
 }
