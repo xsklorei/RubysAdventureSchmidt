89871a1922a0c1f57f281873c70afa808205e705
xsklorei <jcvxka@gmail.com>
2022-11-29 11:42:48 -0800

    Added Script
    Added on to the collectible and just started damage zones and enemies

--- RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
+++ RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 6.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -13,6 +20,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
 
@@ -29,13 +38,18 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
 
         //changing x, y position
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         //set the updated position
         rigidbody2d.MovePosition(position);
     }
 
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 
 
    
