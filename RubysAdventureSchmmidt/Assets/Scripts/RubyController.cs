040d91f8f94f0ca3fe936aaf095abc021da225dd
xsklorei <jcvxka@gmail.com>
2022-12-01 11:44:19 -0800

    Added Damageables
    Finished damageables and starting sprite animation. Ended on #5 need right run, up, and down

--- RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
+++ RubysAdventureSchmmidt/Assets/Scripts/RubyController.cs
@@ -7,9 +7,13 @@ public class RubyController : MonoBehaviour
     public float speed = 6.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 1.0f;
 
     public int health { get { return currentHealth; }}
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -30,6 +34,13 @@ public class RubyController : MonoBehaviour
         //create two variables
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -47,6 +58,18 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+       if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
